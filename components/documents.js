748f628a42b68fd21712fc76d535e93f89b78b4b
bsdaoquang <bsdaoquang@gmail.com>
2020-08-01 15:54:51 +0700

    tạm dừng được rồi

--- components/documents.js
+++ components/documents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 import {styles} from './styles';
 
@@ -7,7 +7,8 @@ export default function Documents(){
   return(
     <View style={styles.container}>
       <View style={styles.inner}>
-        <Text>This is Documents page</Text>
+        <Image source={require('../images/update-doctor.jpg')} resizeMode='stretch' style={{width: '100%', height: 200}}/>
+        <Text style={{margin: 15, color: '#424242', fontsize: 18}}>Xin lỗi bạn! Phần này đang được cập nhật, bạn vui lòng quay lại sau</Text>
       </View>
     </View>
   )
