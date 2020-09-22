f435e2213beef8a04dc91732279c9727d68c35a8
bsdaoquang <bsdaoquang@gmail.com>
2020-09-22 23:16:03 +0700

    chinh lai phan link tải

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, {useEffect} from 'react';
-import { View, Text, Button, Share, Alert, BackHandler } from 'react-native';
+import { View, Text, Button, Share, Alert, BackHandler, Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer'
